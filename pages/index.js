c0c51db14c0fe38ed3e5d44999d27dfe07145e12
wcgordon1 <wcgordon1@gmail.com>
2022-11-28 16:41:19 -0800

    MO MO

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,40 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
-import { useState } from 'react';
 
-const [userInput, setUserInput] = useState('');
+
+const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
 const onUserChangedText = (event) => {
   console.log(event.target.value);
   setUserInput(event.target.value);
 };
 
-const Home = () => {
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
   return (
     <div className="root">
       <Head>
@@ -33,21 +58,31 @@ const Home = () => {
             value={userInput}
             onChange={onUserChangedText}
           />;
-          
-
-      
-        </div>
-      </div>
-      <div className="badge-container grow">
+          {/* New code I added here */}
+  <div className="prompt-buttons">
         <a
-          href="https://will.gives"
-          target="_blank"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>Build with Will</p>
-          </div>
-        </a>
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+      </div>
+    </a>
+        </div>
+         {/* New code I added here */}
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+</div>
       </div>
     </div>
   );
